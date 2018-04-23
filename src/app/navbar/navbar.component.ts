a3243bc9c28af68266bdd85a17149e2aba81edd9
Saravanan Nandhan <sarancruzer@gmail.com>
2018-04-23 16:33:35 +0530

    like unlike changes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -11,81 +11,82 @@ import { Task } from './../_model/task';
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css'],
-  providers:[FavouriteService,TaskService]
+  providers: [FavouriteService, TaskService]
 })
 export class NavbarComponent implements OnInit {
 
-  reactionLists : any = [];  
-  reportLists : any = [];
+  reactionLists: any = [];
+  reportLists: any = [];
 
   model = new Task();
   report: any;
 
-  constructor(private _router:Router,private _favouriteService : FavouriteService, private _service:TaskService) 
-  {
-    this.getReactions();  
-    
+  constructor(private _router: Router, private _favouriteService: FavouriteService, private _service: TaskService) {
+    this.getReactions();
+
     this.model.report_id = null;
-    this.model.report_name = "";
+    this.model.report_name = '';
     this.model.email = false;
     this.model.report = false;
     this.model.daily = false;
-    this.model.delay = "";
+    this.model.delay = '';
     this.model.monthly = false;
-    this.model.days = "";    
+    this.model.days = '';
     this.model.weekly = false;
-    this.model.weekdays = "";
-    this.model.receipients = "";
-    
+    this.model.weekdays = '';
+    this.model.receipients = '';
+
     this.reportLists = [
-      {"id":1,"name":"reportOne"},
-      {"id":2,"name":"reportTwo"},
-      {"id":3,"name":"reportThree"},
-      {"id":4,"name":"reportFour"},
-      {"id":5,"name":"reportFive"},
+      {'id': 1, 'name': 'reportOne'},
+      {'id': 2, 'name': 'reportTwo'},
+      {'id': 3, 'name': 'reportThree'},
+      {'id': 4, 'name': 'reportFour'},
+      {'id': 5, 'name': 'reportFive'},
     ];
 
-    this.model.report_id = "";
+    this.model.report_id = '';
 
 
-    
+
   }
 
   ngOnInit() {
 
-   
+
   }
 
-  changeReport(event: Event){
-    let index = event.target['options'].selectedIndex
-    let selectElementText = event.target['options'][index].text;
-    console.log(selectElementText); 
-    this.model.report_name = selectElementText;            
+  changeReport(event: Event) {
+    const index = event.target['options'].selectedIndex;
+    const selectElementText = event.target['options'][index].text;
+    console.log(selectElementText);
+    this.model.report_name = selectElementText;
   }
 
-  logout(){
-    localStorage.removeItem("employeeName");
-    localStorage.removeItem("email");
-    localStorage.removeItem("employeeNumber");
-    localStorage.removeItem("authentication");
-    localStorage.removeItem("sessions");
+  logout() {
+    localStorage.removeItem('employeeName');
+    localStorage.removeItem('email');
+    localStorage.removeItem('employeeNumber');
+    localStorage.removeItem('authentication');
+    localStorage.removeItem('sessions');
 
-    this._router.navigate(['/login']); 
+    this._router.navigate(['/login']);
   }
 
 
-  getReactions(){
-    this._favouriteService.getReactions().subscribe(response => {
-      let res = response.result.data.info;
-      this.reactionLists = res;
-      localStorage.setItem("reactions",JSON.stringify(res));
-      
-      console.log("currentUser");
-      },
-    err =>{
-    console.log("error msg");
-    //this.donationlistprovider.showErrorToast(err);
-    })
+  getReactions() {
+    this.reactionLists = JSON.parse(localStorage.getItem('reactions'));
+    console.log(this.reactionLists);
+
+    // this._favouriteService.getReactions().subscribe(response => {
+    //   const res = response.result.data.info;
+    //   this.reactionLists = res;
+    //   localStorage.setItem('reactions', JSON.stringify(res));
+    //   console.log('currentUser');
+    //   },
+    // err => {
+    // console.log('error msg');
+    // // this.donationlistprovider.showErrorToast(err);
+    // });
    }
 
    counter(i: number) {
@@ -93,20 +94,20 @@ export class NavbarComponent implements OnInit {
   }
 
 
-  submitTask(form,event : Event){    
+  submitTask(form, event: Event) {
     console.log(event);
     console.log(this.model);
-        
-    if(form.valid){
-      this._service.add(this.model).subscribe(     
+
+    if (form.valid) {
+      this._service.add(this.model).subscribe(
         (res) => {
-              let response = res['result']['info'];              
+              const response = res['result']['info'];
               console.log(response);
         },
-      (err) => { 
+      (err) => {
         console.log(err);
-        console.log("error msg "+err);          
-      }) 
+        console.log('error msg' + err);
+      });
     }
   }
 
