7af2c5f33d84f05bc24f9c9dbef57a2db1ec47a5
kiranvarghesev <kiranvarghesev@gmail.com>
2018-05-04 18:59:00 +0530

    environment changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // apiUrl : 'http://192.168.1.121:5001/',
-  apiUrl : 'http://183.82.33.232:5001/',
+  //apiUrl : 'http://183.82.33.232:5001/',
+  apiUrl:'192.168.1.43:5001'
 };
