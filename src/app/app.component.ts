a38554230c4a98b7b844c5f8483601c3dabbaa5a
Saravanan Nandhan <sarancruzer@gmail.com>
2018-05-09 18:51:51 +0530

    issue changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,25 @@
 import { Component , OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { GlobalSettings } from "./_class/global-settings";
+import { GlobalSettings } from './_class/global-settings';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[GlobalSettings]
+  providers: [GlobalSettings]
 })
 export class AppComponent implements OnInit {
 
-  constructor(private _router: Router,private _globalSettings:GlobalSettings){
+  constructor(private _router: Router, private _globalSettings: GlobalSettings) {
   //  this._router.navigate(['/login']);
   this._globalSettings.authenticated = JSON.parse(localStorage.getItem('authentication'));
-  this._globalSettings.username = localStorage.getItem("firstname");
+  this._globalSettings.username = localStorage.getItem('firstname');
   }
 
   ngOnInit() {
-    console.log("this._globalSettings.authenticated");
+    console.log('this._globalSettings.authenticated');
     console.log(this._globalSettings.authenticated);
-    if(!this._globalSettings.authenticated){
+    if (!this._globalSettings.authenticated) {
         this._router.navigate(['/login']);
     }
   }
